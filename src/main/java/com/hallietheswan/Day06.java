14bed1d31f96321385fb9e35c8890bfe0a40ca3b
Hallie Swan <hallieswan@gmail.com>
2022-12-06 11:36:28 -0800

    day06 part2 ⭐

--- src/main/java/com/hallietheswan/Day06.java
+++ src/main/java/com/hallietheswan/Day06.java
@@ -3,7 +3,6 @@ package com.hallietheswan;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class Day06 implements Day {
 
@@ -13,11 +12,16 @@ public class Day06 implements Day {
         input = Utility.readFileLinesFromResources(fileName).get(0);
     }
 
-    @Override
-    public Object part1() {
+    private int getMessageStartIndex(int nDistinctMessageChars) {
         int left = 0;
         HashMap<Character, Integer> map = new HashMap();
         for (int right = 0; right < input.length(); right++) {
+            // if right - left = nDistinctMessageChars
+            // ...then we're good to go
+            if (right - left == nDistinctMessageChars) {
+                return right;
+            }
+
             char c = input.charAt(right);
             if (map.containsKey(c)) {
                 // move left forward
@@ -26,19 +30,18 @@ public class Day06 implements Day {
 
             // update the entry for this char
             map.put(c, right);
-
-            // if right - left = 4
-            // ...then we're good to go
-            if (right - left == 4) {
-                return right;
-            }
         }
 
         return -1;
     }
 
     @Override
+    public Object part1() {
+        return getMessageStartIndex(4);
+    }
+
+    @Override
     public Object part2() {
-        return -1;
+        return getMessageStartIndex(14);
     }
 }
--- src/test/java/com/hallietheswan/Day06Test.java
+++ src/test/java/com/hallietheswan/Day06Test.java
@@ -11,7 +11,10 @@ class Day06Test {
 
     @Test
     void part1() throws URISyntaxException, IOException {
-        Day06 day06 = new Day06("day06_test1.txt");
+        Day06 day06 = new Day06("day06_test0.txt");
+        assertEquals(7, day06.part1());
+
+        day06 = new Day06("day06_test1.txt");
         assertEquals(5, day06.part1());
 
         day06 = new Day06("day06_test2.txt");
@@ -26,7 +29,19 @@ class Day06Test {
 
     @Test
     void part2() throws URISyntaxException, IOException {
-        Day05 day05 = new Day05("day05_test.txt");
-        assertEquals("MCD", day05.part2());
+        Day06 day06 = new Day06("day06_test0.txt");
+        assertEquals(19, day06.part2());
+
+        day06 = new Day06("day06_test1.txt");
+        assertEquals(23, day06.part2());
+
+        day06 = new Day06("day06_test2.txt");
+        assertEquals(23, day06.part2());
+
+        day06 = new Day06("day06_test3.txt");
+        assertEquals(29, day06.part2());
+
+        day06 = new Day06("day06_test4.txt");
+        assertEquals(26, day06.part2());
     }
 }
